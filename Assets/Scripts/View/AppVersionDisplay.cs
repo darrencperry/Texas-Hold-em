b9d3e10e1a4a9af3bd725b2016d665c38a51da0f
Darren Perry <darrencperry@gmail.com>
2020-05-30 09:07:19 +0100

    - app version display

---
+++ Assets/Scripts/View/AppVersionDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppVersionDisplay : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Text text = GetComponent<Text>();
+        if (text == null) return;
+        text.text = Application.version;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
